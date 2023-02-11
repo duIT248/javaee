d13f19f29ff8d787d73ab1a93a412e0ec26a247d
Knife <dwj20000906>
2023-02-11 13:54:48 +0800

    这是我在IDE上面的第一个测试的小项目！！

--- src/new1/JavaString1.java
+++ src/new1/JavaString1.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class JavaString1 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
-
+        String str = cin.next();
+        for (int i =0 ; i < str.length(); i++){
+            System.out.println(i);
+        }
     }
 }
